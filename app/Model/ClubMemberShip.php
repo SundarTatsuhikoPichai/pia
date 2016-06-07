becb22e9589f3d3899c6d055921fd554ec2ff750
tatsuhikosakamoto <tatsu.husler@gmail.com>
2016-06-07 02:54:11 +0000

    Edit private->public,indent ClubMenberShip.php

--- app/Model/ClubMemberShip.php
+++ app/Model/ClubMemberShip.php
@@ -6,17 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClubMemberShip extends Model
 {
-    //
-    protected $table = 'club_membership';
     protected $guarded = ['id'];
 
-    private function vald($input){
-      $rules = [
-        'club_id' => 'required',
-        'membership_name' => 'required|max:50',
-        'membership_grade'=> 'required|max:10'
-      ];
+    public function vald($input){
+        $rules = [
+            'club_id' => 'required',
+            'membership_name' => 'required|max:50',
+            'membership_grade'=> 'required|max:10'
+        ];
 
-      return Validator::make[$input,$rules];
+        return Validator::make[$input,$rules];
     }
 }
