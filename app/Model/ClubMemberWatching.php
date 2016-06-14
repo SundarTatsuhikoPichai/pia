a6f316dc9a289abfd8482d7b7b74c92379efda70
truthNH <truth.nh@hotmail.co.jp>
2016-06-14 10:59:59 +0900

    Add some models

---
+++ app/Model/ClubMemberWatching.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class ClubMemberWatching extends Model
+{
+    protected $table   = 'club_member_watching';
+    protected $guarded = ['id'];
+
+    public function members() {
+
+        return $this->belongsTo(ClubMembers::class);
+    }
+
+    public function valid($input) {
+        $rules = [
+            'club_member_id'        => 'required',
+            'watched_at'            => 'required|date',
+            'opponent_of_club_code' => 'required|max:10',
+            'home_away'             => 'required|max:10'
+        ];
+
+        return Validator::make($input, $rules);
+    }
+}
--- app/Model/ClubMembers.php
+++ app/Model/ClubMembers.php
@@ -3,8 +3,35 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 class ClubMembers extends Model
 {
-    //
+    protected $table   = 'club_members';
+    protected $guarded = ['id'];
+
+    public function memberWatchings() {
+
+        return $this->hasMany(ClubMemberWatching::class);
+    }
+
+    public function valid($input) {
+        $rules = [
+            'member_id'          => 'required|max:50',
+            'club_id'            => 'required',
+            'year'               => 'required|integer',
+            'sex'                => 'required|digits:1',
+            'birth_day'          => 'required|date',
+            'postal_code'        => 'required|digits:8',
+            'address1'           => 'required|max:50',
+            'address2'           => 'required|max:50',
+            'address3'           => 'required|max:50',
+            'address4'           => 'required|max:50',
+            'first_name_kana'    => 'required|max:50',
+            'last_name_kana'     => 'required|max:50',
+            'club_membership_id' => 'required'
+        ];
+
+        return Validator::make($input, $rules);
+    }
 }
