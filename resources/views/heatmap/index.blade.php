bdd594111a4ad481fd2df4c73fc766d6a537ca34
Masataka Nakagawa <ex3jsb.24karats.masataka@gmail.com>
2016-06-15 16:31:14 +0900

    modified and create views

---
+++ resources/views/heatmap/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('page-title')
+ヒートマップ
+@stop
+
+@section('content')
+Hello Laravel with Blade!!
+@stop
---
+++ resources/views/populationpyramid/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('page-title')
+人口ピラミッド
+@stop
+
+@section('content')
+Hello Laravel with Blade!!
+@stop
