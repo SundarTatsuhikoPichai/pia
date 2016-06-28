556166be65b0e00d1efb410b8724f5ddbe267a56
truthNH <truth.nh@hotmail.co.jp>
2016-06-28 11:01:33 +0900

    Add migration to modify club_members_table

---
+++ database/migrations/2016_06_28_105130_modify_club_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyClubMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('club_members', function ($table) {
+            $table->string('address1', 50)->nullable()->change();
+            $table->date('birth_day', 10)->nullable()->change();
+            $table->string('postal_code', 10)->nullable()->change();
+            $table->string('first_name_kana', 50)->nullable()->change();
+            $table->string('last_name_kana', 50)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('club_members', function ($table) {
+            $table->string('address1', 50)->change();
+            $table->date('birth_day', 10)->change();
+            $table->string('postal_code', 10)->change();
+            $table->string('first_name_kana', 50)->change();
+            $table->string('last_name_kana', 50)->change();
+        });
+    }
+}
---
+++ database/migrations/2016_06_28_105930_modify_club_members_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyClubMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('club_members', function ($table) {
+            $table->string('sex', 1)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('club_members', function ($table) {
+            $table->string('sex', 1)->change();
+        });
+    }
+}
