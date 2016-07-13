d8b9ab9208f8d2a7edca7c098ce730ba92f18e47
tatsuhikosakamoto <tatsu.husler@gmail.com>
2016-07-13 07:06:40 +0000

    Correction sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,33 +3,9 @@
   <!-- sidebar: style can be found in sidebar.less -->
   <section class="sidebar">
 
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel">
-      <div class="pull-left image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-      </div>
-      <div class="pull-left info">
-        <p>Alexander Pierce</p>
-        <!-- Status -->
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-      </div>
-    </div>
-
-    <!-- search form (Optional) -->
-    <form action="#" method="get" class="sidebar-form">
-      <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-      </div>
-    </form>
-    <!-- /.search form -->
 
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
-      <li class="header">HEADER</li>
       <!-- Optionally, you can add icons to the links -->
       <li><a href="{{ URL::asset('/') }}"><i class="fa fa-dashboard"></i><span>ダッシュボード</span></a></li>
       <li><a href="{{ URL::asset('/populationpyramid') }}"><i class="fa fa-align-left"></i><span>人口ピラミッド分析</span></a></li>
