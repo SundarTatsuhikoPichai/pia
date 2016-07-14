0180e644b653bcca87a56564f5ff79c946bd210a
Masataka Nakagawa <ex3jsb.24karats.masataka@gmail.com>
2016-07-14 16:42:48 +0900

    fix inputMemberShip view and controller

--- app/Http/Controllers/InputClubDataController.php
+++ app/Http/Controllers/InputClubDataController.php
@@ -48,4 +48,71 @@ class InputClubDataController extends Controller {
 
         // }
     }
+<<<<<<< Updated upstream
+=======
+
+
+    public function createMemberShip(Request $request) {
+        $rank = $request->input('member_a');
+        $valid = ClubMemberShip::valid($rank);
+
+        if($valid->fails()) {
+            $clubMemberShip = new ClubMemberShip;
+            $clubMemberShip->membership_name = $input['membership_name'];
+        }
+
+        if(!empty(ClubMemberShip::registerClubMembership(json_decode(json_encode($clubMemberShip), true)))){
+                $msg = "正常に保存されました。";
+            } else {
+                $msg = "登録に失敗しました。";
+            }
+    }
+
+    public function update(Request $request) {
+
+        $id = $request->input('id');
+        $memberShipData = ClubMemberShip::find($id);
+
+        var_dump($id);
+        var_dump($request->input('member_a'));
+
+        // if(!$memberShipData->isEmpty()) {
+
+        //     //update(['membership_name' => ]);
+
+
+
+        // }
+
+
+
+
+
+            //Insert clubdata
+            // if(){
+            //     $msg = "正常に更新されました。";
+            // } else {
+            //     $msg = "更新に失敗しました。";
+            // }
+
+            // Return view
+            // $view = view('inputclubdata/updateClubData')->
+            //             with('msg', $msg);
+            // return $view;
+
+        // } else {
+        //     return redirect('/inputclubdata/updateClubData')->withErrors($valid);
+        // }
+    }
+
+
+
+
+
+    //clubs data acquisition
+    public function acquisition(){
+        $clubdatas = Clubs::all();
+        return view('inputclubdata/clubList')->with('clubs', $clubdatas);
+    }
+>>>>>>> Stashed changes
 }
--- public/js/addForm.js
+++ public/js/addForm.js
@@ -1,17 +1,17 @@
 function addrankA()
 {
-     var div_element = $("#inputRankA").children(".form-group:first");
+     var div_element = $("#inputRankA").children(".form-group:last");
      $('#inputRankA').append(div_element.clone(true));
 }
 
 function addrankB()
 {
-     var div_element = $("#inputRankB").children(".form-group:first");
+     var div_element = $("#inputRankB").children(".form-group:last");
      $('#inputRankB').append(div_element.clone(true));
 }
 
 function addrankC()
 {
-     var div_element = $("#inputRankC").children(".form-group:first");
+     var div_element = $("#inputRankC").children(".form-group:last");
      $('#inputRankC').append(div_element.clone(true));
 }
