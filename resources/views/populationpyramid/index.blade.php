3118730a02355bc6c03e240f423e3a281d760f57
truthNH <truth.nh@hotmail.co.jp>
2016-07-18 18:54:35 +0900

    Add population_pyramid_view

--- resources/views/populationpyramid/index.blade.php
+++ resources/views/populationpyramid/index.blade.php
@@ -4,68 +4,145 @@
 クラブ人口ピラミッド
 @stop
 
+@section('addJs')
+<script type="text/javascript" src="{{ asset('bower_components/jquery.narrows.js/jquery.narrows.js') }}"></script>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript">
-    google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(chart);
+  var dataArray = [];
+  $(function() {
+    $("select#club.form-control").narrows("select#year.form-control", {
+      disable_if_parent_is_null: true
+    });
+  });
 
-    function chart()
-    {
-        //var data = new google.visualization.DataTable();
+  $(function() {
+    var club, year, grade;
+    $("select#club, select#year, select#member-grade").change(function() {
+      club  = $("select#club").val();
+      year  = $("select#year").val();
+      grade = $("select#member-grade").val();
+      grade_j = $("select#member-grade option:selected").text();
+      if (club && year && grade) {
+        executeAjax(club, year, grade, grade_j);
+      }
+    }).change();
+  });
 
-        var dataArray = [
-            ['Age', 'Male', 'Female'],
-            ['0-4 years',   106, -104],
-            ['5-9 years',   91,  -86 ],
-            ['10-14 years', 79,  -77 ],
-            ['15-19 years', 68,  -64 ],
-            ['20-24 years', 62,  -58 ],
-            ['25-29 years', 56,  -53 ],
-            ['30-34 years', 51,  -46 ],
-            ['35-39 years', 48,  -41 ],
-            ['40-44 years', 43,  -35 ],
-            ['45-49 years', 39,  -30 ],
-            ['50-54 years', 33,  -27 ],
-            ['55-59 years', 32,  -25 ],
-            ['60-64 years', 27,  -20 ],
-            ['64-69 years', 19,  -16 ],
-            ['70-74 years', 13,  -12 ],
-            ['75-79 years', 8,   -7  ],
-            ['80-84 years', 3,   -3  ],
-            ['85-89 years', 1,   -1  ],
-            ['90-94 years', 0,   0   ],
-            ['95+ years',   0,   0   ]
-                ];
+  function executeAjax(club, year, grade) {
+    $.ajax({
+      type: 'POST',
+      url: '/populationpyramid',
+      data: {
+        "club_name": club,
+        "year": year,
+        "member_grade": grade,
+        "_token": "{!! csrf_token() !!}"
+      },
+      dataType: 'json',
+    })
+    .done(function(data) {;
+      dataArray = data;
+      chart();
+      $('.club-data-1').text(year + '年' + club + ' : ' + grade_j);
+      $('#compare').prop("disabled", false);
+    })
+    .fail(function(data) {;
+      console.log('error');
+    });
+  }
 
-        var data = google.visualization.arrayToDataTable(dataArray);
-
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-        var options = {
-            isStacked: true,
-            hAxis: {
-                format: ';'
-            },
-            vAxis: {
-                direction: -1
-            }
-        };
-
-
-        var formatter = new google.visualization.NumberFormat({
-            pattern: ';'
-        });
-
-        formatter.format(data, 2)
-
-        chart.draw(data, options);
-    }
-
+  google.load("visualization", "1", {packages:["corechart"]});
+  function chart() {
+    var data = google.visualization.arrayToDataTable(dataArray);
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div_1'));
+    var options = {
+      isStacked: true,
+      hAxis: {
+          format: ';'
+      },
+      vAxis: {
+          direction: -1
+      }
+    };
+    var formatter = new google.visualization.NumberFormat({
+        pattern: ';'
+    });
+    formatter.format(data, 2);
+    chart.draw(data, options);
+  }
+  function compare() {
+    $('.club-data-2').text($('.club-data-1').text());
+    var data = google.visualization.arrayToDataTable(dataArray);
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div_2'));
+    var options = {
+      isStacked: true,
+      hAxis: {
+          format: ';'
+      },
+      vAxis: {
+          direction: -1
+      }
+    };
+    var formatter = new google.visualization.NumberFormat({
+        pattern: ';'
+    });
+    formatter.format(data, 2);
+    chart.draw(data, options);
+  }
 </script>
+@stop
 
 @section('content')
 <div clas="row">
-    <div id="chart_div" style="width: 600px; height: 600px;"></div>
+  <div class="box box-primary">
+    <div class="box-header with-border"><h3 class="box-title">条件選択</h3></div>
+    <form role="form" method="post" class="horizontal">
+      <div class="box-body">
+        <div class="form-group col-md-6">
+          <label>クラブ選択</label>
+          <select name="club" class="form-control" id="club">
+            <option value="">-- select --</option>
+            @foreach($clubNames as $clubName)
+            <option value="{{ $clubName }}">{{ $clubName }}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="form-group col-md-6">
+          <label>年度選択</label>
+          <select name="year" class="form-control" id="year">
+            <option value="">-- select --</option>
+            @foreach($clubs as $club)
+            <option value="{{ $club['year'] }}" data-club="{{ $club['club_name'] }}">{{ $club['year'] }}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="form-group col-md-6">
+          <label>会員ランク選択</label>
+          <select name="member-grade" class="form-control" id="member-grade">
+            <option value="">-- select --</option>
+            <option value="A">シーズンパス会員</option>
+            <option value="B">有料会員</option>
+            <option value="C">無料会員</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="box box-primary">
+    <div class="box-header with-border"><h3 class="box-title">人口ピラミッド</h3></div>
+    <div class="box-body">
+      <div class="col-md-6">
+        <h4 class="club-data-1" style="text-align: center;"></h4>
+        <div id="chart_div_1" style="width: 100%; height: 400px;"></div>
+      </div>
+      <div class="col-md-6">
+        <h4 class="club-data-2" style="text-align: center;"></h4>
+        <div id="chart_div_2" style="width: 100%; height: 400px;"></div>
+      </div>
+    </div>
+    <div class="box-footer">
+    <button type="button" id="compare" class="btn btn-primary" onclick="compare()" disabled>比較する</button>
+    </div>
+  </div>
 </div>
-
 @stop
